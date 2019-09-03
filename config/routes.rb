61c41333073ed75ab108fed5423e169c95b52a9e
VerizaWalter <verizawalter@gmail.com>
2019-09-03 14:54:12 +0300

    creating UserMailer after buy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-	resources :items
-	resources :users
-   root to:'items#index'
+#resources :users, only: [:show, :edit, :update]
+resources :items	
+root to:'items#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
