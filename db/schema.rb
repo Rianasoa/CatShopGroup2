15660fc3c615a628defb6d53827cc0b6994a14f3
Miharyrakotonanahary <miharirakotonanahary@gmail.com>
2019-09-04 13:09:08 +0300

    catshop

--- db/schema.rb
+++ db/schema.rb
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_054026) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -60,8 +58,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_054026) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "cart_id"
-    t.index ["cart_id"], name: "index_users_on_cart_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
