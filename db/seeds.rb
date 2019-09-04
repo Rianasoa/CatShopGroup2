e499c7c8927979c0e21d0eea4e7ac4ef73872618
safidy <safidypatrick8@gmail.com>
2019-09-04 09:27:39 +0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,14 @@
 
 
 Item.destroy_all
-
-20.times do
+cart =["https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_960_720.jpg","https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg","https://cdn.pixabay.com/photo/2016/07/10/21/47/cat-1508613__340.jpg","https://cdn.pixabay.com/photo/2015/05/22/05/52/cat-778315__340.jpg","https://cdn.pixabay.com/photo/2016/07/10/21/47/cat-1508613__340.jpg",
+	"https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544__340.jpg","https://cdn.pixabay.com/photo/2017/02/15/12/12/cat-2068462__340.jpg","https://cdn.pixabay.com/photo/2013/04/01/03/45/cat-98359__340.jpg","https://cdn.pixabay.com/photo/2018/01/28/12/37/cat-3113513__340.jpg","https://cdn.pixabay.com/photo/2018/07/08/14/16/cat-3523992__340.jpg","https://cdn.pixabay.com/photo/2016/12/30/12/16/cat-1940487__340.jpg",
+	"https://cdn.pixabay.com/photo/2018/05/30/19/29/cat-3442257__340.jpg"]
+for i in (0..cart.length-1)
 	Item.create!(title: Faker::Creature::Cat.name,
 				description: Faker::Creature::Cat.breed,
 				price: Faker::Commerce.price,
-                image_url: "http://lorempixel.com/640/480/cats")
+                image_url: cart[i])
 end
 
 puts "Items create"
