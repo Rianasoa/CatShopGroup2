96a269bbdd4f1dd0e63479d6b33642c782fc5b9d
Rianasoa <rrianasoaonimalala@gmail.com>
2019-09-05 09:38:59 +0300

    add back

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jquery-rails'
 gem 'faker'
+gem 'table_print'
+gem 'stripe'
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
 	belongs_to :user, optional: true
 	has_many :items
+
+	after_create :order_send
+
+	# This method calls the order_email method from user_mailer, passing the order as an argument
+	def order_send
+		UserMailer.order_email(self).deliver_now
+		AdminMailer.order_email(self).deliver_now
+	end
 end
