6a409b8ec5af7dec9f16c45f49398f7fe46a1564
Miharyrakotonanahary <miharirakotonanahary@gmail.com>
2019-09-06 09:56:40 +0300

    Front sign in and footer

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 
 	after_create :order_send
 
-	# This method calls the order_email method from user_mailer, passing the order as an argument
+	 #This method calls the order_email method from user_mailer, passing the order as an argument
 	def order_send
 		UserMailer.order_email(self).deliver_now
 		AdminMailer.order_email(self).deliver_now
